32fa538f37574d47906b5206483bf33af0f94601
jestra03 <joshemestra@gmail.com>
2023-10-18 20:43:04 -0700

    build heap added, perc up added

--- heap_tests.py
+++ heap_tests.py
@@ -8,8 +8,8 @@ from heap import *
 
 class TestHeap(unittest.TestCase):
     def test_standard(self):
-        test_heap = MaxHeap(50)
-
+        heap = MaxHeap(50)
+        heap.build_heap([1, 20, 9, 81, 2, 27, 15, 3])
 
 if __name__ == "__main__":
     unittest.main()
