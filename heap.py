75499b7d6a2a704aa7d9331b631e59443a506fb9
jestra03 <joshemestra@gmail.com>
2023-10-19 19:27:09 -0700

    haha first try streakgit push

--- heap.py
+++ heap.py
@@ -5,7 +5,7 @@
 class MaxHeap:
     def __init__(self, capacity=50):
         self.capacity = capacity
-        self.heap = [None] + [None]*capacity  # allocate new arr for heap (0 index not included)
+        self.heap = [None] + [None] * capacity  # allocate new arr for heap (0 index not included)
         self.size = 0
 
     def enqueue(self, item):
--- heap_tests.py
+++ heap_tests.py
@@ -18,7 +18,11 @@ class TestHeap(unittest.TestCase):
         heap.enqueue(15)
         heap.enqueue(3)
         self.assertEqual(heap.contents(), [81, 20, 27, 3, 2, 9, 15, 1])
+        heap.dequeue()
+        self.assertEqual(heap.contents(), [27, 20, 15, 3, 2, 9, 1])
 
+    def test_build_heap(self):
+        heap = MaxHeap()
         heap.build_heap([1, 20, 9, 81, 2, 27, 15, 3])
         exp = [81, 20, 27, 3, 2, 9, 15, 1]  # done by hand (white-boarding)
         self.assertEqual(heap.contents(), exp)
